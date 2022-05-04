e8e401b8d91a75457abf699c035679238dbfa73d
macarron001 <rpinoon16@gmail.com>
2022-05-05 04:55:35 +0800

    added rspec feat test for task editing

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", type: :request do
+
+  describe "visit new task page" do
+
+    let(:category) { Category.create!(title: "title", description: "description") }
+    before { visit "/categories/#{category.id}/tasks/new" }
+    
+    it 'shows a new page' do
+      expect(page).to have_content 'Body'
+      expect(page).to have_selector(:link_or_button, "Create Task")
+    end
+  end
+
+  describe "create a new task" do
+
+    let(:category) { Category.create!(title: "title", description: "description") }
+    before { visit "/categories/#{category.id}/tasks/new" }
+    
+    it 'submits a form' do
+      within 'form' do
+        fill_in 'task_body', with: "test"
+        click_on "Create Task"
+      end
+      expect(Task.count).to eq(1)
+    end
+  end
+
+  describe "show a task" do
+
+    let(:category) { Category.create!(title: "title", description: "description") }
+    let(:task) {category.tasks.create!(body: "task")}
+    before { visit "/categories/#{category.id}/tasks/#{task.id}" }
+    
+    it 'shows a task page' do
+      expect(page).to have_content 'Body'
+      expect(page).to have_selector(:link_or_button, "Edit this task")
+    end
+  end
+
+  describe "edit a task" do
+
+    let(:category) { Category.create!(title: "title", description: "description") }
+    let(:task) {category.tasks.create!(body: "task")}
+    before { visit "/categories/#{category.id}/tasks/#{task.id}/edit" }
+    
+    it 'submits a form' do
+      within 'form' do
+        fill_in 'task_body', with: "test"
+        click_on "Update Task"
+      end
+      
+      edited_task = category.tasks.find(task.id)
+      expect(edited_task.body).to eq("test")
+    end
+  end
+
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let!(:task) {Task.new}
+  let!(:category) {Category.new}
+  let!(:task) {category.tasks.new}
 
   context 'Validations' do
+    it 'should create a valid task' do
+      task.body = 'Test'
+
+      expect(task).to be_valid
+    end
+
     it 'is not valid without a body' do
       task.body = nil
 
